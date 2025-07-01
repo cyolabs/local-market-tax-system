fc0ab56987993fe81b25d83d2841a0f416e8474c
WinnyAnyango <anyangowinny905@gmail.com>
2025-07-01 16:35:37 +0300

    Ensured foced ssl connection when connecting to database

--- backend/taxbackend/taxbackend/settings.py
+++ backend/taxbackend/taxbackend/settings.py
@@ -75,7 +75,8 @@ WSGI_APPLICATION = 'taxbackend.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'),conn_max_age=600,
+        ssl_require=True )
 }
 
 
