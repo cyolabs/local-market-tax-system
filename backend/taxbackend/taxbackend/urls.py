e1aeddf4fff511a39a2738db4a7d485efee6e396
WinnyAnyango <anyangowinny905@gmail.com>
2025-07-22 13:37:37 +0300

    Added JWT paths to the correct urls.py

--- backend/taxbackend/taxbackend/urls.py
+++ backend/taxbackend/taxbackend/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from django.http import HttpResponse
 from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 def home(request):
     return HttpResponse("""
@@ -16,5 +21,8 @@ def home(request):
 urlpatterns = [
     path('', home, name='home'), 
     path('admin/', admin.site.urls),
-    path('', include('tax_api.urls')),  
+    path('', include('tax_api.urls')), 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
