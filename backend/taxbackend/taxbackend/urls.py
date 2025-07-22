8e0e8feac7f90bfb29282a5eb69a150db9a709d1
WinnyAnyango <anyangowinny905@gmail.com>
2025-07-22 12:36:58 +0300

    Quickfix fix ensuring JWTAuthentication works

--- backend/taxbackend/taxbackend/urls.py
+++ backend/taxbackend/taxbackend/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from django.http import HttpResponse
 from django.contrib import admin
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 def home(request):
     return HttpResponse("""
@@ -10,11 +14,15 @@ def home(request):
             <li><a href="/signup/">/signup/</a> - User registration</li>
             <li><a href="/login/">/login/</a> - User login</li>
             <li><a href="/admin/">Admin Panel</a></li>
+            <li><a href="/api/token/">/api/token/</a> - Obtain JWT token</li>
+            <li><a href="/api/token/refresh/">/api/token/refresh/</a> - Refresh JWT token</li>
         </ul>
     """)
 
 urlpatterns = [
-    path('', home, name='home'), 
-    path('admin/', admin.site.urls),
-    path('', include('tax_api.urls')),  
+    path('', home, name='home'),
+    path('admin/', admin.site.urls),  
+    path('api/', include('tax_api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
