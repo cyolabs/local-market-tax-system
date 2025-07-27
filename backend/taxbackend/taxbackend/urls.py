09465633d2a26e510cc55f3ce8323e5d32dabb1d
WinnyAnyango <anyangowinny905@gmail.com>
2025-07-27 11:31:38 +0300

    Updated url root

--- backend/taxbackend/taxbackend/urls.py
+++ backend/taxbackend/taxbackend/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('tax_api.urls')),
-    path('mpesa', include('mpesa_express.urls')),
+    path('mpesa/', include('mpesa_express.urls')),
 ]
