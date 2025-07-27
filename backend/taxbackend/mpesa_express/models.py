22fb684300bc09a5ab4f0f66982a22eb9c68cc07
WinnyAnyango <anyangowinny905@gmail.com>
2025-07-27 18:02:43 +0300

    Fixing checout id in models

--- backend/taxbackend/mpesa_express/models.py
+++ backend/taxbackend/mpesa_express/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    cheout_id = models.CharField(max_length=100, unique=True)
+    checkout_id = models.CharField(max_length=100, unique=True)
     mpesa_code = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=15)
     status = models.CharField(max_length=20)
